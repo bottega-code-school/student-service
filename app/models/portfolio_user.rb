16fd8ecdcdc36ae8d765b9d2e2923a5ac4d93d08
jordanhudgens <jordan@devcamp.com>
2020-04-29 11:55:41 -0700

    Built out the portfolio user model

--- app/models/portfolio_user.rb
+++ app/models/portfolio_user.rb
@@ -1,2 +1,20 @@
 class PortfolioUser < ApplicationRecord
+  has_secure_password
+
+  validates_presence_of :email
+  validates_uniqueness_of :email, scope: :client_id
+
+  belongs_to :client
+
+  def can_modify_user?(user_id)
+    id.to_s == user_id.to_s
+  end
+
+  def column_names_merged_with_images
+    PortfolioUser.column_names
+      .select { |column| !['client_id', 'password_digest', 'updated_at'].include?(column) }
+      .push(['id', 'email', 'password', 'password_confirmation'])
+      .flatten
+      .uniq
+  end
 end
