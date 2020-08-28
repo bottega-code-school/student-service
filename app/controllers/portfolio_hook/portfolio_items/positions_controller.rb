a307d2b5e04f0b550854e1db935156feb771b6ec
jordanhudgens <jordan@devcamp.com>
2020-08-28 14:28:17 -0700

    Built out the portfolio positions controller

---
+++ app/controllers/portfolio_hook/portfolio_items/positions_controller.rb
@@ -0,0 +1,24 @@
+class PortfolioHook::PortfolioItems::PositionsController < PortfolioHookController
+  before_action :set_portfolio_item
+  before_action :authenticate_portfolio_user
+  include AuthenticationConcern
+  include ClientFromSubdomainConcern
+
+  def update
+    if @current_client && @portfolio_item.client == @current_client
+      if @portfolio_item.update(portfolio_item_params)
+        render json: @portfolio_item, status: :created
+      else
+        render json: @portfolio_item.errors, status: :unprocessable_entity
+      end
+    else
+      render json: { status: :unauthorized }
+    end
+  end
+
+  private
+
+    def set_portfolio_item
+      @portfolio_item = PortfolioItem.find(params[:portfolio_item_id])
+    end
+end
