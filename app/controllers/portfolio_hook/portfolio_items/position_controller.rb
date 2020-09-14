2b8f417c1cb712b449c80197bdfacfc6edaa1320
jordanhudgens <jordan@devcamp.com>
2020-09-14 13:56:35 -0700

    Added auth debugging

--- app/controllers/portfolio_hook/portfolio_items/position_controller.rb
+++ app/controllers/portfolio_hook/portfolio_items/position_controller.rb
@@ -5,6 +5,9 @@ class PortfolioHook::PortfolioItems::PositionController < PortfolioHookControlle
   include ClientFromSubdomainConcern
 
   def update
+    puts "current_client" * 100, @current_client.inspect, "current_client" * 100
+    puts "portfolio_item" * 100, @portfolio_item.inspect, "portfolio_item" * 100
+    puts "portfolio_item client" * 100, @portfolio_item.client.inspect, "portfolio_item client" * 100
     if @current_client && @portfolio_item.client == @current_client
       if @portfolio_item.update(position_position: params[:position])
         render json: @portfolio_item, status: :updated
