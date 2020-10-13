e46a4cde59706a96f426dff6963ef2908adeea37
Jordan Hudgens <jordan@devcamp.com>
2020-10-13 09:17:52 -0700

    Updated the position controller auth rules

--- app/controllers/portfolio_hook/portfolio_items/position_controller.rb
+++ app/controllers/portfolio_hook/portfolio_items/position_controller.rb
@@ -6,7 +6,7 @@ class PortfolioHook::PortfolioItems::PositionController < PortfolioHookControlle
     puts "current_portfolio_user" * 100, current_portfolio_user.inspect, "current_portfolio_user" * 100
     puts "portfolio_item" * 100, @portfolio_item.inspect, "portfolio_item" * 100
     puts "portfolio_item client" * 100, @portfolio_item.client.inspect, "portfolio_item client" * 100
-    if current_portfolio_user && @portfolio_item.client == current_portfolio_user.client
+    if @portfolio_item.client == current_client
       if @portfolio_item.update(position_position: params[:position])
         render json: @portfolio_item, status: :updated
       else
